5835f3b55136ceef75978099dcc6d9a280334b30
Noman <noman@noman.land>
2022-09-03 17:43:32 -0400

    Make tabular

--- src/Idiots.tsx
+++ src/Idiots.tsx
@@ -1,41 +1,98 @@
 import idiots from './json/idiots.json';
 import { StatePopulations } from './States';
 
+type CaseStatus =
+  | 'acquitted'
+  | 'convicted'
+  | 'dismissed'
+  | 'missing'
+  | 'pleaded-guilty'
+  | 'pleaded-not-guilty';
+
+type Jurisdiction = 'Federal' | 'DC' | 'Federal and DC';
 type YesNo = 'Y' | 'N';
 
 type Idiot = {
-  first: string;
-  last: string;
-  middle: string;
-  publish: YesNo;
-  new: YesNo;
-  updated: YesNo;
-  summary: string;
-  jurisdiction: 'Federal' | 'DC';
+  age: number;
+  case_status: CaseStatus;
+  case_updates: string;
   charges: string;
   charges_link: string;
-  case_status: 'pleaded-guilty' | 'pleaded-not-guilty';
-  case_updates: string;
-  violence_assault: YesNo;
-  conspiracy: YesNo;
-  theft: YesNo;
-  property: YesNo;
-  age: number;
   city: string;
-  state: keyof StatePopulations;
-  military_le: YesNo;
+  conspiracy: YesNo;
+  first: string;
   extremist: YesNo;
   inspired_trump: YesNo;
+  jurisdiction: Jurisdiction;
+  last: string;
+  middle: string;
+  military_le: YesNo;
+  new: YesNo;
   photo_name: string;
+  property: YesNo;
+  publish: YesNo;
+  summary: string;
+  state: keyof StatePopulations;
+  theft: YesNo;
+  updated: YesNo;
+  violence_assault: YesNo;
 };
 
+type Field = {
+  prop: keyof Idiot;
+  label: string;
+};
+
+const fields: Field[] = [
+  {
+    prop: 'first',
+    label: 'First',
+  },
+  {
+    prop: 'last',
+    label: 'Last',
+  },
+  {
+    prop: 'age',
+    label: 'Age',
+  },
+  {
+    prop: 'city',
+    label: 'City',
+  },
+  {
+    prop: 'state',
+    label: 'State',
+  },
+  {
+    prop: 'case_status',
+    label: 'Status',
+  },
+  {
+    prop: 'jurisdiction',
+    label: 'Jusrisdiction',
+  },
+];
+
 export const Idiots = () => (
-  <div>
-    {(idiots as Idiot[]).map(idiot => (
-      <div key={`${idiot.first}-${idiot.last}-${idiot.charges_link}`}>
-        {idiot.first} {idiot.last}, aged {idiot.age}, from {idiot.city},{' '}
-        {idiot.state}.
-      </div>
-    ))}
-  </div>
+  <table>
+    <thead>
+      {fields.map(({ label }) => (
+        <th align="left" key={label}>
+          {label}
+        </th>
+      ))}
+    </thead>
+    <tbody>
+      {(idiots as Idiot[])
+        .sort((iA, iB) => iB.age - iA.age)
+        .map(idiot => (
+          <tr key={`${idiot.first}-${idiot.last}-${idiot.charges_link}`}>
+            {fields.map(({ prop }) => (
+              <td key={prop}>{idiot[prop] || '--'}</td>
+            ))}
+          </tr>
+        ))}
+    </tbody>
+  </table>
 );
--- src/States.tsx
+++ src/States.tsx
@@ -6,14 +6,27 @@ export type StatePopulations = {
   [K in State]: number;
 };
 
+const formatter = new Intl.NumberFormat().format;
+
+const sortedStates = Object.entries(statePopulations as StatePopulations).sort(
+  ([, popA], [, popB]) => popB - popA
+);
+
 export const States = () => (
   <div>
-    {Object.entries(statePopulations as StatePopulations).map(
-      ([state, population]) => (
-        <div key={state}>
-          {state}: {population}
-        </div>
-      )
-    )}
+    <table>
+      <thead>
+        <th align="left">State</th>
+        <th align="right">Population</th>
+      </thead>
+      <tbody>
+        {sortedStates.map(([state, population]) => (
+          <tr key={state}>
+            <td>{state}</td>
+            <td align="right">{formatter(population)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   </div>
 );
